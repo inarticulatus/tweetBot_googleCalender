16fd482fa94a0677e8ee7533bce42c225ca744dd
utkarsh7599 <utkarsh1999tripathi@gmail.com>
2017-01-01 12:57:56 +0530

    Update tweet_event.bat

--- tweet_event.bat
+++ tweet_event.bat
@@ -1,7 +1,13 @@
-:ping
-ping 1.2.3.4 -n 1 -w 1000 > null
+@echo off
+:loop
+ping 1.2.3.4 -n 1 -w 1000 > nul
 set target=www.google.com
 ping %target% -n 1 | find "TTL="
 if errorlevel==1 goto ping
+
 chdir /d %cd%
 python googleCalendarAPI_quickstart.py
+timeout /t 3 > nul
+python TweetBot.py
+timeout /t 3600 > nul
+goto loop
